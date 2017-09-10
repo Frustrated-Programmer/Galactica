25d170f8fb80670804de8c7d5e767a3c932e5ccd
TemporalFuzz <vanxmax@gmail.com>
2017-09-10 13:30:11 -0700

    Create colors.js

---
+++ colors.js
@@ -0,0 +1,7 @@
+module.exports = {
+  purple: 0x993499,//Moderation
+  yellow: 0xadb60c,//Research
+  red: 0xce001f,//Invalid, Something Bad
+  blue: 0x00C8C8,//Game Notifications
+  green: 0x09c612//Confirmed, Something Bad
+};
