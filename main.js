9eac5680d7d58c235b1f8ae0b5a2ed7647cd780f
TemporalFuzz <vanxmax@gmail.com>
2017-09-11 16:11:36 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,59 +3,93 @@
  */
 var version = "0.0.0";
 
-var fs = require("fs");
-var Discord = require("discord.js");
-var client = new Discord.Client();
-var JsonAccounts = require("./Accounts.json");
+const fs = require("fs");
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var accountData = require("./Accounts.json");
 
 
 
-//varibles
-var embedColors = {
-    yellow: 0xadb60c,//MODERATION/RESEARCHING
-    red:0xce001f,//INVALID ITEM
-    lightBlue:0x00C8C8,//NORMAL GAME ITEMS
-    green:0x09c612//SOMETHING GOOD
-};
-var defaultPrefix = "-";//this prefix will work globally! and is the prefix used in DM
-var commands =[
+//Constants
+const embedColors = require("./colors.js");
+const universalPrefix = "-";
+const commands = [
     {
-        names:["version"],
-        description:"Gives you the current version",
-        usage:"version",
-        events:{},
-        does:function(message,words){
+        names: ["version"],
+        description: "Gives you the current version",
+        requirements: [],
+        effect: function(message, args){
             var embed = new Discord.RichEmbed()
                 .setColor(embedColors.green)
-                .setDescription("Version is `"+version+"`");
+                .setTitle("Galactica | Version | " + version);
+            message.channel.send(embed);
+        }
+    },
+    {
+        names: ["version"],
+        description: "Gives you the current version",
+        requirements: [],
+        effect: function(message, args){
+            var embed = new Discord.RichEmbed()
+                .setColor(embedColors.green)
+                .setTitle("Galactica | Version | " + version);
             message.channel.send(embed);
         }
     }
 ];
 
-//clients
-client.on("ready", function () {
-    console.log("Bot is ready to take commands");
-    client.user.setGame(defaultPrefix + 'help||Guilds: ' + (client.guilds.size));
+var eventChecks = {
+    "argNum": function(eventArgs, message, args) {
+        return args[eventArgs][0] !== parseInt(args[eventArgs][0], 10);
+    },
+    "argOver": function(eventArgs, message, args) {
+        if(eventChecks.argNum(eventArgs, message, args)) return false;
+        return args[eventArgs[0]] > parseInt(eventArgs[1]);
+    },
+    "argUnder": function(eventArgs, message, args) {
+        if(eventChecks.argNum(eventArgs, message, args)) return false;
+        return args[eventArgs[0]] < parseInt(eventArgs[1]);
+    },
+    "argNot": function(eventArgs, message, args) {
+        if(eventChecks.argNum(eventArgs, message, args)) return false;
+        return args[eventArgs[0]] !== parseInt(eventArgs[1]);
+    },
+};
 
+function runCommand(command, message, args) {
+    for(var i = 0; i < command.events.length; i++) {
+        var typeEvent = command.events[i].split(" ")[0];
+        var eventArgs = command.events[i].split(" ");
+        eventArgs.shift();
+        
+        if(!eventChecks[typeEvent](eventArgs, message, args)) return;
+    }
+    
+    command.effect(message, args);
+    return;
+}
+
+client.on("ready", function () {
+    console.log("Galactica | Online");
+    client.user.setGame(defaultPrefix + 'help | Guilds: ' + (client.guilds.size));
 });
+
 client.on("message", function (message) {
-    if (message.author.bot) {//if its a bot
+    if (message.author.bot) {
         return;
     }
-    var words = message.content.toLowerCase();
-    words = words.split(" ");
+    
+    var command = message.content.toLowerCase.split(" ")[0];
+    var args = message.content.toLowerCase().split(" ");
+    args.shift();
 
     for (var i = 0; i < commands.length; i++) {
         for (var j = 0; j < commands[i].names.length; j++) {
-            if(commands[i].names[j].toLowerCase()===defaultPrefix+words[0]){
-                commands[i].does(message,words);
-                return;
+            if(commands[i].names[j].toLowerCase() === universalPrefix + words[0]){
+                runCommand(commands[0], message, args);
             }
         }
     }
 });
 
-//token
-var tok = require("./config.json");
-client.login(tok.token);
+client.login(require("./config.json").token);//Secure Login
