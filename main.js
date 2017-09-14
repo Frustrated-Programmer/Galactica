8e84ba0f5fd271a868ec35d9a52edd3070e0fa36
Frustrated-Programmer <eliassantiagoanderson@gmail.com>
2017-09-13 22:19:46 -0500

    updated the -warp command cleaned up code a little bit added a checkPerms function made the bot check for SEND_MESSAGES and EMBED_LINKS perms

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 /**
- * Created by Elijah on 9/9/2017.
- */
+©TemporalFuzz
+©FrustratedProgrammer
+**/
 var version = "0.0.0";
-
 const fs = require("fs");
 const Discord = require("discord.js");
 const client = new Discord.Client();
@@ -13,6 +13,8 @@ const client = new Discord.Client();
 var accountData = require("./accounts.json").players;
 var UpdateAccount = require("./account.js");
 var listOfPages = [];
+var listOfWaitTimes = [];
+var waitTimesInterval = false;
 
 
 /**CONSTANTS**/
@@ -70,9 +72,14 @@ const commands = [
             if (playerData != null) {
                 var embed = new Discord.RichEmbed()
                     .setTitle(message.member.displayName+"'s stats")
-                    .setColor(embedColors.blue)
-                    .addField("Location:","Galaxy `"+playerData.location[0]+"` Area: `"+playerData.location[1]+"x"+playerData.location[2]+"`");
-                message.channel.send(embed);
+                    .setFooter(playerData.id)
+                    .setColor(embedColors.blue);
+                if(typeof playerData.location === "object") {
+                    embed.addField("Location:", "Galaxy `" + (playerData.location[0] + 1) + "` Area: `" + playerData.location[1] + "x" + playerData.location[2] + "`");
+                }else{
+                    embed.addField("Location:",playerData.location);
+                }
+                    message.channel.send(embed);
             } else {
                 var newPlayerData = new UpdateAccount();
                 newPlayerData.userID=message.author.id;
@@ -108,27 +115,139 @@ const commands = [
             });
         }
     },
+    {
+        names: ["warp","go"],
+        description: "warp to somewhere",
+        reqs: ["profile"],
+        effect: function (message, args, playerData) {
+            if(typeof playerData.location === "object") {
+                var numbers = getNumbers(message.content);
+                var warpType, goToPos = playerData.location;
+                switch (numbers.length) {
+                    default:
+                        warpType = "Invalid";
+                        break;
+                    case 1:
+                        warpType = "galaxy";
+                        goToPos[0] = numbers[0];
+                        break;
+                    case 2:
+                        warpType = "positionBase";
+                        goToPos[1] = numbers[0];
+                        goToPos[2] = numbers[1];
+                        break;
+                    case 3:
+                        warpType = "galaxyAndPosition";
+                        goToPos = numbers;
+                        break;
+                }
+                if (warpType === "Invalid") {
+                    sendBasicEmbed({content: "Invalid usage", color: embedColors.red, channel: message.channel})
+                }
+                else {
+                    var timeUntilFinishedWarping = 0;
+                    if (goToPos[1] + 1 > playerData.location[1]) {
+                        timeUntilFinishedWarping += goToPos[1] + 1 - playerData.location[1];
+                    }
+                    else {
+                        timeUntilFinishedWarping += playerData.location[1] - goToPos[1] + 1;
+                    }
+                    if (goToPos[2] + 1 > playerData.location[2]) {
+                        timeUntilFinishedWarping += goToPos[2] + 1 - playerData.location[2];
+                    }
+                    else {
+                        timeUntilFinishedWarping += playerData.location[2] - goToPos[2] + 1;
+                    }
+                    if (warpType === "galaxy") {
+                        timeUntilFinishedWarping = 0;
+                    }
+                    if (warpType !== "positionBase") {
+                        timeUntilFinishedWarping += 60 * 5;//5 mins if its a galaxy warp
+                    }
+                    timeUntilFinishedWarping = timeUntilFinishedWarping * 1000;//convert it into actual Date.now()
+                    listOfWaitTimes.push({
+                        player: playerData,
+                        expires: Date.now() + timeUntilFinishedWarping,
+                        headTo: goToPos,
+                        type: "warp"
+                    });
+                    if (!waitTimesInterval) {
+                        waitTimesInterval = setInterval(checkWaitTimes, 1000);//once every second
+                    }
+                    playerData.location = "Warping to Galaxy: `" + (goToPos[0] + 1) + "` Area: `" + goToPos[1] + "x" + goToPos[2] + "`";
+                    sendBasicEmbed({
+                        content: "Warping will take approximately: " + getTimeRemaining(timeUntilFinishedWarping),
+                        color: embedColors.blue,
+                        channel: message.channel
+                    })
+                }
+            }
+            else{
+                var timeLeft = null;
+                for(var i =0;i<listOfWaitTimes.length;i++){
+                    if(playerData.id === listOfWaitTimes[i].player.id){
+                        timeLeft = listOfWaitTimes[i].expires-Date.now();
+                        break;
+                    }
+                }
+                sendBasicEmbed({
+                    content:"You are currently warping.\nYour warp will finish in approximately "+getTimeRemaining(timeLeft),
+                    channel:message.channel,
+                    color:embedColors.yellow
+                })
+
+            }
+        }
+    }
 ];
 const reqChecks = {
-    "argNum": function (reqArgs, message, args) {
+    "argNum": function (reqArgs, message, args, playerData) {
         return args[reqArgs][0] !== parseInt(args[reqArgs][0], 10);
     },
-    "argOver": function (reqArgs, message, args) {
-        if (reqChecks.argNum(reqArgs, message, args)) return false;
+    "argOver": function (reqArgs, message, args, playerData) {
+        if (reqChecks.argNum(reqArgs, message, args, playerData)) return false;
         return args[reqArgs[0]] > parseInt(reqArgs[1]);
     },
-    "argUnder": function (reqArgs, message, args) {
-        if (reqChecks.argNum(reqArgs, message, args)) return false;
+    "argUnder": function (reqArgs, message, args, playerData) {
+        if (reqChecks.argNum(reqArgs, message, args, playerData)) return false;
         return args[reqArgs[0]] < parseInt(reqArgs[1]);
     },
-    "argNot": function (reqArgs, message, args) {
-        if (reqChecks.argNum(reqArgs, message, args)) return false;
+    "argNot": function (reqArgs, message, args, playerData) {
+        if (reqChecks.argNum(reqArgs, message, args, playerData)) return false;
         return args[reqArgs[0]] !== parseInt(reqArgs[1]);
     },
+    "botPerms": function (reqArgs, message, args, playerData) {
+        return checkPerms({
+            user: "bot",
+            perms: reqArgs[0],
+            message: message
+        });
+    },
+    "isntWarping": function (reqArgs, message, args, playerData) {
+        return typeof playerData.location === "object";
+    },
+    "profile":function(reqArgs, message, args, playerData) {
+        return playerData;
+    }
 };
 
 
 /**FUNCTIONS**/
+function checkWaitTimes(){
+    for(var i =0;i<listOfWaitTimes.length;i++){
+        if(listOfWaitTimes[i].expires<Date.now()){
+            switch(listOfWaitTimes[i].type){
+                case "warp": listOfWaitTimes[i].player.location = listOfWaitTimes[i].headTo;break;
+            }
+            console.log("finished");
+            listOfWaitTimes.splice(i,1)
+        }
+    }
+    if(!listOfWaitTimes.length){
+        clearInterval(waitTimesInterval);
+        waitTimesInterval = false;
+    }
+}
 function createMap(galaxys, xSize, ySize) {
     var planets = [
         {
@@ -189,7 +308,7 @@ function findPlayerData(ID) {
 }
 function saveJsonFile(file) {
     fs.writeFileSync(file, JSON.stringify(require(file), null, 4));//the (null, 4) "cleans" up the json file
-};
+}
 function sendBasicEmbed(args) {
     var embed = new Discord.RichEmbed()
         .setColor(args.color)
@@ -206,17 +325,17 @@ function channelClear(channel, msgnum) {
             }
         });
     }
-};
-function runCommand(command, message, args) {
+}
+function runCommand(command, message, args,playerData) {
     for (var i = 0; i < command.reqs.length; i++) {
         var typeReq = command.reqs[i].split(" ")[0];
         var reqArgs = command.reqs[i].split(" ");
         reqArgs.shift();
 
-        if (!reqChecks[typeReq](reqArgs, message, args)) return;
+        if (!reqChecks[typeReq](reqArgs, message, args,playerData)) return;
     }
 
-    command.effect(message, args, findPlayerData(message.author.id));
+    command.effect(message, args, playerData);
     return;
 }
 function getNumbers(text,parsed) {
@@ -246,7 +365,63 @@ function getNumbers(text,parsed) {
         }
     }
     return wordsWithNumbers;
-};//insert in text get back an array of all the numbers in that text
+}//insert in text get back an array of all the numbers in that text
+function getTimeRemaining(time) {
+    var times = [[86400000, 0, "day"], [3600000, 0, "hour"], [60000, 0, "minute"], [1000, 0, "second"], [1, 0, "millisecond"]];
+    var timeLeftText = "";
+    var fakeTime = time;
+    for (var i = 0; i < times.length; i++) {
+        if (fakeTime >= times[i][0]) {
+            while (fakeTime >= times[i][0]) {
+                fakeTime -= times[i][0];
+                times[i][1]++;
+            }
+        }
+        if (times[i][1] > 0) {
+            timeLeftText += "`"+times[i][1] + "` ";
+            timeLeftText += times[i][2];
+            if (times[i][1] > 0) {
+                timeLeftText += "s";
+            }
+            if(i===times.length){
+                timeLeftText+=" and "
+            }else{
+                timeLeftText+=", "
+            }
+        }
+    }
+    return timeLeftText;
+
+
+}
+function checkPerms(args){
+    var permsCheck = {
+        channelPerms:false,
+        serverPerms:false
+    };//To check wether the channel is overriding it or not
+
+    var user;
+    if(args.user === "bot"){
+        user = args.message.client.user;
+    }else if(args.user === "user"){
+        user = message.member;
+    }else{
+        console.log("args.user should be \"user\" or \"bot\"");
+        return false;
+    }
+    if (args.message.channel.permissionsFor(user).has(args.perms, true)) {
+        permsCheck.channelPerms=true;
+    }//does it have channel perms
+    if(args.message.guild.members.get(user.id).hasPermission(args.perms,[null,true,false])){
+        permsCheck.serverPerms = true;
+    }//does it have server perms
+    if(permsCheck.serverPerms!=true){
+        return false;
+    }else if(permsCheck.channelPerms!=true){
+        return false;
+    }
+    return true;
+}
 
 
 /**CLIENTS**/
@@ -267,7 +442,27 @@ client.on("message", function (message) {
     for (var i = 0; i < commands.length; i++) {
         for (var j = 0; j < commands[i].names.length; j++) {
             if (universalPrefix + commands[i].names[j].toLowerCase() === command) {
-                runCommand(commands[i], message, args);
+                if(message.channel.type!=="dm"){
+                    //SEND_MESSAGES
+                    if(!checkPerms({user:"bot",perms:"SEND_MESSAGES",message:message})) {
+                        sendBasicEmbed({
+                            content:"I do not have `SEND_MESSAGES` permission",
+                            color:embedColors.red,
+                            channel:message.author
+                        });
+                        return;
+                    }
+                    //EMBED_LINKS
+                    if(!checkPerms({user:"bot",perms:"EMBED_LINKS",message:message})) {
+                        sendBasicEmbed({
+                            content:"I do not have `EMBED_LINKS` permission",
+                            color:embedColors.red,
+                            channel:message.author
+                        });
+                        return;
+                    }
+                }
+                runCommand(commands[i], message, args, findPlayerData(message.author.id));
             }
         }
     }
