5c0bfbac2f79ab811f97f59c9840613bac914f84
Frustrated-Programmer <eliassantiagoanderson@gmail.com>
2017-10-02 22:39:00 -0500

    added pink for stations

--- colors.js
+++ colors.js
@@ -1,7 +1,8 @@
 module.exports = {
-  purple: 0x993499,//Moderation
-  yellow: 0xadb60c,//Research
-  red: 0xce001f,//Invalid, Something Bad
-  blue: 0x00C8C8,//Game Notifications
-  green: 0x09c612//Confirmed, Something Bad
+    purple: 0x993499,//Moderation
+    yellow: 0xadb60c,//Research
+    pink: 0xFF21F8,//stations
+    red: 0xce001f,//Invalid, Something Bad
+    blue: 0x00C8C8,//Game Notifications
+    green: 0x09c612//Confirmed, Something Good
 };
