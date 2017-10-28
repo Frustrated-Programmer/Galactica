679443d3152f93b6ce40180427ffdc1af0be7747
Frustrated-Programmer <eliassantiagoanderson@gmail.com>
2017-10-28 16:34:34 -0500

    added `help COMMAND`

--- items.js
+++ items.js
@@ -189,7 +189,7 @@ const timeTakes = {
 	 *  600000 = 10 minutes
 	 * 3600000 =  1 hour
 	 */
-	colonize        : 60000,
+	colonize        : 60000*5,
 	attackColony    : 60000 * 10,
 	buildStation    : 60000 * 5,
 	attackStation   : 60000 * 10,
